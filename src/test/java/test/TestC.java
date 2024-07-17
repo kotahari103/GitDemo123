76e247a58fc0aa85656cac06d4725f571394930e
harikrishna <kotahari103@gmail.com>
2024-07-17 12:21:43 +0530

    first commit

---
+++ src/test/java/test/TestC.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.testng.Assert;
+
+public class TestC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("TestC");
+		Assert.assertTrue(false);
+
+	}
+
+}
